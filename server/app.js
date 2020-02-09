f54455b7634615387dc8e1678fba671b2f4e6590
shawnSFU <shawn.noruzi@gmail.com>
2020-02-09 12:58:48 -0800

    Added ability to search graph for a book, added graphiql interface to localhost

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql'); 
+const schema = require('./schema/schema');
 
 const app = express();
 
 //setting up middleware
 app.use('/graphql', graphqlHTTP({
+	//scheme: schema but in es6 just schema
+	schema,
+	graphiql: true
 }));
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,18 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 //describes data,object types, relations between object types, 
 //and how we can access the data (mutate,query)
 
-//destructure function from graphQL 
-const {GraphQLObjectType, GraphQLString} = graphql;
+//destructure functions/props from graphQL package
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+//dummy data
+var books = [
+	{name:'a', genre:'Fantasy',id:'1'},
+	{name:'b', genre:'Romance',id:'2'},
+	{name:'c', genre:'Horror',id:'3'}
+]
 
 //declare new object type
 //declaring fields helps avoid reference errors 
@@ -17,4 +25,28 @@ const BookType = new GraphQLObjectType({
 	})
 });
 
+//grab books - authors - all books - all authors
+//not worried about order here -> just access into the different parts of our graph.
+//args = which arguments should come when along with the user's query + their data type
+//resolve = contains code to get data we need from db/other source
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		book: {
+			//what we expect when user queries book
+			type: BookType,
+			args: { id: { type: GraphQLString } },
+			//what is returned
+			resolve(parent, args) {
+				//args.id exists in this scope
+				//search books array and find the book with the ID 
+				//books is dummy data -> will be replaced by database later
+				return _.find(books, { id: args.id });
+			}
+		}
+	}
+});
 
+module.exports = new GraphQLSchema({
+	query: RootQuery
+});
