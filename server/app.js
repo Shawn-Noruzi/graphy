f873fa896907018e6cf951954378d4f5c046539d
shawnSFU <shawn.noruzi@gmail.com>
2020-02-08 21:38:26 -0800

    Creationg of graphQL endpoint

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql'); 
 
 const app = express();
+
+//setting up middleware
+app.use('/graphql', graphqlHTTP({
+}));
 
 app.listen(4000, () => {
 	console.log('listening for requests on port 4000');
