6629b90f8b5ab0c37be6d81ff57b1afaeec88621
shawnSFU <shawn.noruzi@gmail.com>
2020-02-09 23:03:13 -0800

    Creation of models, added mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const Book = require("../models/book");
+const Author = require("../models/author");
 
 //Purpose : describes data, object types, relations between object types,
 //and how we can access the data (mutate,query)
@@ -19,22 +21,6 @@ const {
   GraphQLList
 } = graphql;
 
-//dummy data
-const books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
-  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
-  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
-  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" }
-];
-
-const authors = [
-  { name: "Patrick Rothfuss", age: 44, id: "1" },
-  { name: "Brandon Sanderson", age: 42, id: "2" },
-  { name: "Terry Pratchett", age: 66, id: "3" }
-];
-
 //declare new object type
 //declaring fields helps avoid reference errors
 const BookType = new GraphQLObjectType({
@@ -47,7 +33,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       resolve(parent, args) {
         console.log(parent);
-        return _.find(authors, { id: parent.authorId });
+        //return _.find(authors, { id: parent.authorId });
       }
     }
   })
@@ -63,7 +49,7 @@ const AuthorType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       resolve(parent, args) {
         //return any books with an id that matches the authorId given in the 'author' query
-        return _.filter(books, { authorId: parent.id });
+        //return _.filter(books, { authorId: parent.id });
       }
     }
   })
@@ -89,7 +75,7 @@ const RootQuery = new GraphQLObjectType({
         //args.id exists in this scope
         //search books array and find the book with the ID
         //books is dummy data -> will be replaced by database later
-        return _.find(books, { id: args.id });
+        //return _.find(books, { id: args.id });
       }
     },
     author: {
@@ -103,13 +89,13 @@ const RootQuery = new GraphQLObjectType({
       //we're going to grab the authorId from Author data -> return all books with matching authorId
       //when user queries for author(id:"3") -> they get book x,y,z data back.
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        //return _.find(authors, { id: args.id });
       }
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return books;
+        //return books;
       }
     },
     authors: {
@@ -121,6 +107,30 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+//Adding new authors
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        //create local instance of Author object (imported from model)
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        });
+        //save to DB
+        return author.save();
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
