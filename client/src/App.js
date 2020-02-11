8bb85736468b1922a28696f11771296510fdf7ad
shawnSFU <shawn.noruzi@gmail.com>
2020-02-10 20:52:20 -0800

    Connected Apollo and our GraphQL server inside our React App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+//components
 import BookList from "./components/BookList";
+
+//apollo client setup
+const client = new ApolloClient({
+  //endpoint we make requests to
+  uri: "http://localhost:4000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="main">
-        <BookList/>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="main">
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
